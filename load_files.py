d56e598675543809e1d893333afa20cb2846ca24
Dennis Qiu <dennisqiu123@gmail.com>
2024-04-01 16:17:08 -0400

    Added files directory check

--- load_files.py
+++ load_files.py
@@ -3,6 +3,8 @@ import os
 from utils.comparator import ExcelComparator
 
 files_directory = os.path.join(os.path.dirname(__file__), 'files')
+if not os.path.exists(files_directory):
+    os.mkdir(files_directory)
 
 files = (
     ExcelComparator(excel_path=os.path.join(files_directory, 'prod.xlsx'), sheet_name='prod', start_row=1),
